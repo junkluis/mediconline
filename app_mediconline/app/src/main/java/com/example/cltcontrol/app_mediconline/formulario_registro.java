0426e863a82cb78985361360db6bdc1cec27b3f8
junkluis <lufezuro@gmail.com>
2019-05-30 12:52:52 -0500

    mediconline

---
+++ app_mediconline/app/src/main/java/com/example/cltcontrol/app_mediconline/formulario_registro.java
@@ -0,0 +1,57 @@
+package com.example.cltcontrol.app_mediconline;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class formulario_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_registro);
+    }
+
+    //insertarpaciente
+
+    public void insertarpaciente(View v) {
+        addNotification();
+
+    }
+
+
+    private void addNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.aa)
+                        .setContentTitle("Se ha registrado con exito")
+                        .setContentText("Revise su correo electronico en los proximos 2 dias")
+                        .setAutoCancel(true)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+        Intent notificationIntent = new Intent(this, formulario_registro.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        notificationIntent.putExtra("message", "This is a notification message");
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+
+
+
+
+}
